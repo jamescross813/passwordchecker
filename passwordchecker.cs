3210542aadb7a7d6b4bbd119473d19b59d76e965
jamescross813 <crossjames813@gmail.com>
2023-07-23 21:53:31 -0500

    basic program and vars

---
+++ passwordchecker.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace PasswordChecker
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      int minLength = 8;
+      string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string lowerCase = "abcdefghijklmnopqrstuvwkyz";
+      string digits = "1234567890";
+      string specialChars = "!=%;&_-#$^*@";
+
+      
+      }
+
+      
+    }
+  }
+}
